774920b131d88a705e97db4058f0d493eea78a12
Ethan Gyllenhaal <egyllenhaal@unm.edu>
2020-08-27 21:14:03 -0600

    Added comments
    Better late than never?

--- trim_fasta.py
+++ trim_fasta.py
@@ -1,46 +1,40 @@
 import re, sys, getopt
 
 def main(argv):
-    # initializing variables
+    # gets inputs
     in_path = ''
     out_path = ''
     loci_path = ''
-    # read command line input
-    # -h or -help for basic usage info
-    # -i or --input for input fasta
-    # -l or --loci for locus file, a text file where each row contains a locus name (generally uce)
-    # -o or --output for output location for trimmed fasta, assuming format is ">[locus][other text]\n[sequence]\n"
     try:
         opts, args = getopt.getopt(argv,"hi:l:o:",["input=","loci=","output="])
     except:
-        print 'trim_fasta.py -i [input] -l [loci] -o [output]'
-        print 'Input is a fasta reference. Loci is a text file where each row contains a locus name (generally uce).'
-        print 'The input should have the format ">[locus][other text]\\n[sequence]\\n".'
+        print 'trim_fasta.py -i [input] -loci [loci] -o [output]'
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-help", "-h"):
-            print 'trim_fasta.py -i [input] -l [loci] -o [output]'
-            print 'Input is a fasta reference. Loci is a text file where each row contains a locus name (generally uce).'
-            print 'The input should have the format ">[locus][other text]\\n[sequence]\\n".'
+            print 'trim_fasta.py -i [input] -loci [loci] -o [output]'
+            sys.exit()
         elif opt in ("-i", "--input"):
             in_path = arg
         elif opt in ("-l", "--loci"):
             loci_path = arg
         elif opt in ("-o", "--output"):
             out_path = arg
-    
-    # reads input file as one string
+            
+    # opens input fasta
     with open(in_path, 'r') as inFile:
         input = inFile.read()
+    # initializes array for the loci
     loci=[]
-    # reads in loci
+    # reads in loci not to include, from BLAST
     with open(loci_path) as text:
         loci = text.readlines()
-    # replaces each locus assuming format is >[locus][other text]\n[sequence]\n
+    # succinct, but replaces any locus name found in locus input file
+    # not very memory efficient, for larger files loop through each contig in fasta and see if name matches anything in loci to remove then write directly to output
     for i in loci:
         replace = '>' + i.rstrip() + '(.*)\\n([A-Z]|[a-z]|\\n)*'
         input = re.sub(replace, '', input)
-    # writes output
+    # writes
     with open(out_path, 'w') as outFile:
         outFile.write(input)
 if __name__ == '__main__':
